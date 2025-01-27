31e12401669a01dfa7f6cc05441532a85999071d
Fernando Costa <fgosuendacosta@gmail.com>
2025-01-27 08:39:36 -0300

    feat: creating addFriend function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let friendsList = [];
+
+function addFriend() {
+  const input = document.getElementById("friend");
+  const name = input.value.trim();
+
+  if (!name) {
+    alert("Please enter a valid name.");
+    return;
+  }
+
+  friendsList.push(name);
+
+  input.value = "";
+
+  displayList();
+}
+
